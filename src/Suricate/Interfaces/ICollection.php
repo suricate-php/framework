e92e41c848c94313eb11b3abf1d76efc5655b927
Mathieu LESNIAK <mathieu@lesniak.fr>
2014-03-04 22:38:38 +0100

    [Fix] Namespace

--- src/Suricate/Interfaces/ICollection.php
+++ src/Suricate/Interfaces/ICollection.php
@@ -7,9 +7,9 @@ interface ICollection
 {
     const SORT_ASC              = 'ASC';
     const SORT_DESC             = 'DESC';
-    
+
     public static function loadAll();
-    
+
     public static function buildFromSql($sql, $sqlParams = array());
 
     public static function loadForParentId($parentId);
@@ -19,31 +19,31 @@ interface ICollection
     public function lazyLoadFromSql($sql, $sqlParams = array());
 
     public function setParentIdForAll($parentId);
-    
+
     public function craftItem($itemData);
-    
+
     public function save();
 
     public function purgeItems();
 
     public function sort($field, $order);
-    
+
     public function getPossibleValuesFor($args, $withMapping = true);
-    
+
     public function getValuesFor($name);
-    
+
     public function getItems();
-    
+
     public function addItemLink($link_id);
 
-    public function addItem(Fwk\DBObject $item);
-    
+    public function addItem(DBObject $item);
+
     public function getItemsType();
-    
+
     public function getParentIdName();
-    
+
     public function getParentId();
-    
+
     public function getSlice($start, $nbItems = null);
 
     public function getFirstItem();
