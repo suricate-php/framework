94092b46ea242155fb8fa840da478c95ba8091fb
Mathieu LESNIAK <maverick@eskuel.net>
2015-01-17 15:47:25 +0100

    [upg] headers

--- src/Suricate/Exception/HttpException.php
+++ src/Suricate/Exception/HttpException.php
@@ -5,15 +5,23 @@ namespace Suricate\Exception;
 class HttpException extends \RuntimeException
 {
     private $statusCode;
+    private $headers;
     
-    public function __construct($statusCode, $message = null, \Exception $previous = null, $code = 0)
+    public function __construct($statusCode, $message = null, \Exception $previous = null, array $headers = array(), $code = 0)
     {
         $this->statusCode   = $statusCode;
+        $this->headers      = $headers;
+
         parent::__construct($message, $code, $previous);
     }
 
     public function getStatusCode()
     {
         return $this->statusCode;
+    }
+
+    public function getHeaders()
+    {
+        return $this->headers;
     }
 }
