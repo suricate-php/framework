2f21dfbf254742be3685e6dabffdd45b5fbce0a9
Mathieu LESNIAK <maverick@eskuel.net>
2015-02-13 23:24:41 +0100

    [Upg] Split + dispatch() method + match protocol

--- src/Suricate/Route.php
+++ src/Suricate/Route.php
@@ -3,7 +3,11 @@ namespace Suricate;
 
 class Route
 {
-    private $url;
+    private $name;
+    private $method;
+    private $path;
+    private $computedPath;
+
     private $parametersDefinitions;
     public $parametersValues;
 
@@ -17,62 +21,138 @@ class Route
     const ROUTE_DEFAULT_CONTROLLER  = 'home';
     const ROUTE_DEFAULT_METHOD      = 'index';
 
-    public function __construct($url, $requestUri, $routeTarget, $parametersDefinitions = array(), $middleware = null)
+    public function __construct($name, $method, $path, $request, $routeTarget, $parametersDefinitions = array(), $middleware = null)
     {
+
         $this->isMatched                = false;
-        $this->url                      = $url;
+        $this->name                     = $name;
+        $this->method                   = $method;
+        $this->path                     = $path;
+        $this->target                   = $routeTarget;
         $this->parametersDefinitions    = $parametersDefinitions;
         $this->parametersValues         = array();
-        if ($middleware !== null) {
+        $this->middlewares              = (array)$middleware;
 
-        }
-        $this->middlewares               = (array)$middleware;
+        $this->setParameters();
+        $this->computePath();
+        $this->match($request);
+    }
+
+    /**
+     * Get route path
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    private function match($request)
+    {
+        $requestUri = $request->getRequestUri();
 
         if (($pos = strpos($requestUri, '?')) !== false) {
             $requestUri = substr($requestUri, 0, $pos);
         }
 
-        // Get all route parameters
-        preg_match_all('|:([\w]+)|', $url, $routeParameters);
-        $routeParametersNames = $routeParameters[1];
-        
-        $patternMatching = $this->url;
+        if ($this->method == 'any' || strtolower($this->method) == strtolower($request->getMethod())) {
+            // requestUri is matching pattern, set as matched route
+            if (preg_match('#^' . $this->computedPath . '$#', $requestUri, $matching)) {
 
-        // Patterns parameters are not set, considering implicit declaration
+                foreach (array_keys($this->parametersDefinitions) as $currentParameter) {
+                    $this->parametersValues[$currentParameter] = isset($matching[$currentParameter]) ? $matching[$currentParameter] : null;
+                }
+
+                
+                $this->isMatched        = true;
+            }
+        }
+    }
+
+    public function dispatch(&$response)
+    {
+        $result     = false;
+        $callable   = $this->getCallable($response);
+
+        if (is_callable($callable)) {
+            // We found a valid method for this controller
+            // Find parameters order
+            $methodArguments = $this->getCallableArguments();
+
+            // Calling $controller->method with arguments in right order
+            $result = true;
+            call_user_func_array($callable, $methodArguments);
+            
+            foreach ($this->middlewares as $middleware) {
+
+            }
+        }
+
+        return $result;
+    }
+
+    private function getCallable(&$response)
+    {
+        if (count($this->target) > 1) {
+            $callable = array(
+                new $this->target[0]($response, $this),
+                $this->target[1]
+            );
+        } else {
+            $callable = $this->target;
+        }
+
+        return $callable;
+    }
+
+    private function getCallableArguments()
+    {
+        if (count($this->target) > 1) {
+            $reflection = new \ReflectionMethod($this->target[0], $this->target[1]);
+        } else {
+            $reflection = new \ReflectionFunction($this->target);
+        }
         
+        $methodParameters = $reflection->getParameters();
+        $methodArguments = array();
+
+        foreach ($methodParameters as $index => $parameter) {
+            if (isset($this->parametersValues[$parameter->name])) {
+                $methodArguments[$index] = urldecode($this->parametersValues[$parameter->name]);
+            } else {
+                // No value matching this parameter
+                $methodArguments[$index] = null;
+            }
+        }
+
+        return $methodArguments;
+    }
+
+    protected function setParameters()
+    {
+        // Get all route parameters
+        preg_match_all('|:([\w]+)|', $this->path, $routeParameters);
+        $routeParametersNames = $routeParameters[1];
+
         foreach ($routeParametersNames as $parameter) {
+            // Patterns parameters are not set, considering implicit declaration
             if (!isset($this->parametersDefinitions[$parameter])) {
                 $this->parametersDefinitions[$parameter] = '.*';
             }
         }
-        
-        /**
-        // TODO : no parameters ?
-        **/
-        
-        // Assigning parameters
-        foreach ($this->parametersDefinitions as $parameterName => $parameterDefinition) {
-            $patternMatching = str_replace(':' . $parameterName, '(?<' . $parameterName . '>' . $parameterDefinition . ')', $patternMatching);
-        }
-    
-        /**
-         TODO : match protocol
-         */
-        
-        // requestUri is matching pattern, set as matched route
-        if (preg_match('#^' . $patternMatching . '$#', $requestUri, $matching)) {
-
-            foreach ($routeParametersNames as $currentParameter) {
-                $this->parametersValues[$currentParameter] = isset($matching[$currentParameter]) ? $matching[$currentParameter] : null;
-            }
-
-            $this->target           = $routeTarget;
-            $this->isMatched        = true;
-        }
     }
 
-    public function getUrl()
+    /**
+     * Build PCRE pattern path, according to route parameters
+     * @return null
+     */
+    protected function computePath()
     {
-        return $this->url;
+        $this->computedPath = $this->path;
+
+        // Assigning parameters
+        foreach ($this->parametersDefinitions as $parameterName => $parameterDefinition) {
+            $this->computedPath = str_replace(':' . $parameterName, '(?<' . $parameterName . '>' . $parameterDefinition . ')', $this->computedPath);
+        }
     }
 }
