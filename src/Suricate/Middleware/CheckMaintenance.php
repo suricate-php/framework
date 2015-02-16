0c3487ce7d91651725c2ccd2465787cbcf5c64d8
Mathieu LESNIAK <maverick@eskuel.net>
2015-02-16 22:15:13 +0100

    [Upg] Maintenance middleware

--- src/Suricate/Middleware/CheckMaintenance.php
+++ src/Suricate/Middleware/CheckMaintenance.php
@@ -3,8 +3,10 @@ namespace Suricate\Middleware;
 
 class checkMaintenance
 {
-    public function handle($response)
+    public function call(&$response)
     {
-        
+        if (app()->inMaintenance()) {
+            app()->abort(503, 'Maintenance in progress');
+        }
     }
 }
