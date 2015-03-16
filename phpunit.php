23053d914142930c53023b2888988a9bcd274386
Mathieu LESNIAK <mathieu@lesniak.fr>
2015-03-16 13:34:56 +0100

    [Fix] Typo

--- phpunit.php
+++ phpunit.php
@@ -1,8 +1,7 @@
 <?php
-set_include_path(dirname(__FILE__)  . PATH_SEPARATOR . get_include_path());
+set_include_path(dirname(__FILE__).'/'  . PATH_SEPARATOR . get_include_path());
 
-var_dump(getcwd(), get_include_path());
-require_once 'src/Suricate/Autoloader.php';
+require_once 'src/Suricate/AutoLoader.php';
 
 \Suricate\AutoLoader::register();
 $app = new Suricate\Suricate();
