b9065fa6174747ecc87416e5cdd1801a9f406b8a
Mathieu LESNIAK <maverick@eskuel.net>
2017-06-04 16:49:30 +0200

    [Fix] PHPUnit classnames
    classnames moved with latest PHPUnit (now in PSR)
    Alias old one to keep compatibility

--- phpunit.php
+++ phpunit.php
@@ -3,5 +3,9 @@ set_include_path(dirname(__FILE__).'/'  . PATH_SEPARATOR . get_include_path());
 
 require_once 'src/Suricate/AutoLoader.php';
 
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')) {
+  class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
+
 \Suricate\AutoLoader::register();
 $app = new Suricate\Suricate();
