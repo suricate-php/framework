457562e0784834c5797e8f41089f66166011ddc6
Mathieu LESNIAK <maverick@eskuel.net>
2017-06-04 17:06:24 +0200

    [Upg] Add more Collection tests

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -19,11 +19,61 @@ class CollectionTest extends PHPUnit_Framework_TestCase {
         
     }
 
+    public function testCount()
+    {
+        $arr = array(1, 2, 3);
+        $collection = new \Suricate\Collection($arr);
+        $this->assertEquals(3, $collection->count());
+    }
+
+    public function testFirst()
+    {
+        $arr = array(4, 5, 6);
+        $collection = new \Suricate\Collection($arr);
+        $this->assertEquals(4, $collection->first());
+    }
+
+    public function testLast()
+    {
+        $arr = array(1, 2, 3);
+        $collection = new \Suricate\Collection($arr);
+        $this->assertEquals(3, $collection->last());
+
+
+        $collection = new \Suricate\Collection();
+        $this->assertEquals(null, $collection->last());
+    }
+
+    public function testSum()
+    {
+        $arr = array(1, 2, 3);
+        $collection = new \Suricate\Collection($arr);
+        $this->assertEquals(6, $collection->sum());
+    }
+
     public function testHas()
     {
         $arr = array('a' => 1, 'b' => 2, 'c' => 3);
         $collection = new \Suricate\Collection($arr);
         $this->assertEquals(true, $collection->has('b'));
         $this->assertEquals(false, $collection->has('d'));
+    }
+
+    public function testFilter()
+    {
+        $arr = array('a' => 1, 'b' => 2, 'c' => 3);
+        $collection = new \Suricate\Collection($arr);
+        $this->assertEquals(['b' => 2], $collection->filter(
+            function ($value, $key) {
+                return ($value % 2) === 0;
+            })
+        );
+    }
+
+    public function testPush()
+    {
+        $arr = array(1, 2, 3);
+        $collection = new \Suricate\Collection($arr);
+        $this->assertEquals([1, 2, 3, 4], $collection->push(4));
     }
 }
