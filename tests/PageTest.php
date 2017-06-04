fe5d3aff8482761516378c595c3d1df7cec55da8
Mathieu LESNIAK <mathieu@lesniak.fr>
2017-06-04 17:50:44 +0200

    add tests

--- tests/PageTest.php
+++ tests/PageTest.php
@@ -7,4 +7,19 @@ class PageTest extends PHPUnit_Framework_TestCase
         $page->setLanguage('fr_FR');
         $this->assertAttributeEquals('fr_FR', 'language', $page);
     }
+
+    public function testEncoding()
+    {
+        $page = new \Suricate\Page();
+        $page->setEncoding('iso-8859-1');
+        $this->assertAttributeEquals('iso-8859-1', 'encoding', $page);
+    }
+
+    public function testTitle()
+    {
+        $page = new \Suricate\Page();
+        $page->setTitle('My great webpage');
+        $this->assertAttributeEquals('My great webpage', 'title', $page);
+    }
+
 }
