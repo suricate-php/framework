440d25706c58a5fe6f6982737e56e490b19f0310
Mathieu LESNIAK <mathieu@lesniak.fr>
2017-10-07 19:22:05 +0200

    add tests

---
+++ tests/AppTest.php
@@ -0,0 +1,35 @@
+<?php
+class AppTest extends PHPUnit_Framework_TestCase {
+    
+    public function testIsDebug()
+    {
+        $app = new \Suricate\App();
+        $app->mode = \Suricate\App::DEBUG_MODE;
+        
+        $this->assertTrue($app->isDebug());
+    }
+
+    public function testIsDevelopment()
+    {
+        $app = new \Suricate\App();
+        $app->mode = \Suricate\App::DEVELOPMENT_MODE;
+        
+        $this->assertTrue($app->isDevelopment());
+    }
+
+    public function testIsPrelive()
+    {
+        $app = new \Suricate\App();
+        $app->mode = \Suricate\App::PRELIVE_MODE;
+        
+        $this->assertTrue($app->isPrelive());
+    }
+
+    public function testIsProduction()
+    {
+        $app = new \Suricate\App();
+        $app->mode = \Suricate\App::PRODUCTION_MODE;
+        
+        $this->assertTrue($app->isProduction());
+    }
+}
