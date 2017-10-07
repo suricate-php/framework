1a6d40ce4c4bb3e9219be0495ce0185f10260055
Mathieu LESNIAK <mathieu@lesniak.fr>
2017-10-07 19:33:13 +0200

    add tests

--- tests/ServiceTest.php
+++ tests/ServiceTest.php
@@ -1,9 +1,18 @@
 <?php
 class ServiceTest extends PHPUnit_Framework_TestCase {
     
-    public function testGetter()
+    public function testGetException()
     {
-        
+        $this->expectException(InvalidArgumentException::class);
+        $service = new \Suricate\Service();
+        $service->undefVar;
+    }
+
+    public function testSetException()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $service = new \Suricate\Service();
+        $service->undefVar = "123";
     }
 
 }
