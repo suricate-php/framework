caa5153dc6196242afdd0720f136e0b80bf2c35b
Mathieu LESNIAK <mathieu@lesniak.fr>
2019-01-13 22:09:28 +0100

    short array notation + type hinting

--- src/Suricate/Request.php
+++ src/Suricate/Request.php
@@ -20,84 +20,83 @@ class Request
             self::HTTP_METHOD_OPTIONS   => 'OPTIONS'
         );
 
-    private $httpCodeString = array(
-                    100 => 'Continue',
-                    101 => 'Switching Protocols',
-                    102 => 'Processing',
-                    200 => 'OK',
-                    201 => 'Created',
-                    202 => 'Accepted',
-                    203 => 'Non-Authoritative Information',
-                    204 => 'No Content',
-                    205 => 'Reset Content',
-                    206 => 'Partial Content',
-                    207 => 'Multi-Status',
-                    208 => 'Already Reported',
-                    226 => 'IM Used',
-                    250 => 'Low on Storage Space',
-                    300 => 'Multiple Choices',
-                    301 => 'Moved Permanently',
-                    302 => 'Found',
-                    303 => 'See Other',
-                    304 => 'Not Modified',
-                    305 => 'Use Proxy',
-                    306 => '306 Switch Proxy',
-                    307 => 'Temporary Redirect',
-                    308 => 'Permanent Redirect',
-                    400 => 'Bad Request',
-                    401 => 'Unauthorized',
-                    402 => 'Payment Required',
-                    403 => 'Forbidden',
-                    404 => 'Not Found',
-                    405 => 'Method Not Allowed',
-                    406 => 'Not Acceptable',
-                    407 => 'Proxy Authentication Required',
-                    408 => 'Request Timeout',
-                    409 => 'Conflict',
-                    410 => 'Gone',
-                    411 => 'Length Required',
-                    412 => 'Precondition Failed',
-                    413 => 'Request Entity Too Large',
-                    414 => 'Request-URI Too Long',
-                    415 => 'Unsupported Media Type',
-                    416 => 'Requested Range Not Satisfiable',
-                    417 => 'Expectation Failed',
-                    422 => 'Unprocessable Entity',
-                    423 => 'Locked',
-                    424 => 'Failed Dependency',
-                    425 => 'Unordered Collection',
-                    426 => 'Upgrade Required',
-                    428 => 'Precondition Required',
-                    429 => 'Too Many Requests',
-                    431 => 'Request Header Fields Too Large',
-                    444 => 'No Response',
-                    449 => 'Retry With',
-                    450 => 'Blocked by Windows Parental Controls',
-                    494 => 'Request Header Too Large',
-                    495 => 'Cert Error',
-                    496 => 'No Cert',
-                    497 => 'HTTP to HTTPS',
-                    499 => 'Client Closed Request',
-                    500 => 'Internal Server Error',
-                    501 => 'Not Implemented',
-                    502 => 'Bad Gateway',
-                    503 => 'Service Unavailable',
-                    504 => 'Gateway Timeout',
-                    505 => 'HTTP Version Not Supported',
-                    506 => 'Variant Also Negotiates',
-                    507 => 'Insufficient Storage',
-                    508 => 'Loop Detected',
-                    509 => 'Bandwidth Limit Exceeded',
-                    510 => 'Not Extended',
-                    511 => 'Network Authentication Required'
-        );
+    private $httpCodeString = [
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',
+        208 => 'Already Reported',
+        226 => 'IM Used',
+        250 => 'Low on Storage Space',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => '306 Switch Proxy',
+        307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        444 => 'No Response',
+        449 => 'Retry With',
+        450 => 'Blocked by Windows Parental Controls',
+        494 => 'Request Header Too Large',
+        495 => 'Cert Error',
+        496 => 'No Cert',
+        497 => 'HTTP to HTTPS',
+        499 => 'Client Closed Request',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        509 => 'Bandwidth Limit Exceeded',
+        510 => 'Not Extended',
+        511 => 'Network Authentication Required'
+    ];
 
     private $httpCode;
     private $headers;
     private $requestUri;
     private $url;
     private $body;
-    private $flashData = array();
     private $path;
     private $query;
 
@@ -185,11 +184,12 @@ class Request
     {
         if (isset($_GET[$variable])) {
             return $_GET[$variable];
-        } elseif (isset($_POST[$variable])) {
-            return $_POST[$variable];
-        } else {
-            return $defaultValue;
         }
+        if (isset($_POST[$variable])) {
+            return $_POST[$variable];
+        }
+        
+        return $defaultValue;
     }
 
     public static function hasParam($variable)
@@ -218,24 +218,24 @@ class Request
     }
 
 
-    public function setContentType($content_type, $encoding = null)
+    public function setContentType(string $contentType, $encoding = null): Request
     {
         if ($encoding !== null) {
-            $content_type .= '; charset=' . $encoding;
+            $contentType .= '; charset=' . $encoding;
         }
-        $this->addHeader('Content-type', $content_type);
+        $this->addHeader('Content-type', $contentType);
 
         return $this;
     }
 
-    public function setBody($body)
+    public function setBody(string $body): Request
     {
         $this->body = $body;
 
         return $this;
     }
 
-    public function getBody()
+    public function getBody(): string
     {
         return $this->body;
     }
@@ -273,7 +273,7 @@ class Request
     // HTTP Code
     //
 
-    public function setHttpCode($code)
+    public function setHttpCode($code): Request
     {
         $this->httpCode = $code;
 
@@ -336,22 +336,42 @@ class Request
             ->redirect($url);
     }
 
-    public function isOK()
+    /**
+     * Check if request has a 200 OK Code
+     *
+     * @return boolean
+     */
+    public function isOK(): bool
     {
         return ( $this->httpCode == 200 );
     }
 
-    public function isRedirect()
+    /**
+     * Check if request has a 3XX HTTP code
+     *
+     * @return boolean
+     */
+    public function isRedirect(): bool
     {
         return ( $this->httpCode >= 300 && $this->httpCode < 400 );
     }
 
-    public function isClientError()
+    /**
+     * Check is request has a 4XX HTTP code
+     *
+     * @return boolean
+     */
+    public function isClientError(): bool
     {
         return ( $this->httpCode >= 400 && $this->httpCode < 500 );
     }
 
-    public function isServerError()
+    /**
+     * Check if request has a 5XX HTTP code
+     *
+     * @return boolean
+     */
+    public function isServerError(): bool
     {
         return ( $this->httpCode >= 500 && $this->httpCode < 600 );
     }
