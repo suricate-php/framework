a14d8322773a0acb15f87891a905f667a2bd5b41
Mathieu LESNIAK <mathieu@lesniak.fr>
2019-01-15 21:50:39 +0100

    dbojbect dbproperties

---
+++ tests/DBObjectTest.php
@@ -0,0 +1,57 @@
+<?php
+class DBObjectTest extends PHPUnit_Framework_TestCase
+{
+    public function testContructor()
+    {
+        $classname = '\Suricate\DBObject';
+
+        // Get mock, without the constructor being called
+        $mock = $this->getMockBuilder($classname)
+            ->disableOriginalConstructor()
+            ->setMethods(array('setRelations'))
+            ->getMockForAbstractClass();
+
+      // set expectations for constructor calls
+      $mock->expects($this->once())
+          ->method('setRelations');
+
+      // now call the constructor
+      $reflectedClass = new ReflectionClass($classname);
+      $constructor = $reflectedClass->getConstructor();
+      $constructor->invoke($mock);
+    }
+
+    public function testUndefinedGet()
+    {
+        $testDBO = new \Suricate\DBObject();
+        self::mockProperty($testDBO, 'dbVariables', ['id', 'name', 'last_update']);
+        $this->expectException(\InvalidArgumentException::class);
+        
+        $testDBO->undefinedVar;
+    }
+
+    public function testDBProperty()
+    {
+        $testDBO = new \Suricate\DBObject();
+        $testDBO->regularProperty = 42;
+        self::mockProperty($testDBO, 'dbVariables', ['id', 'name', 'not_loaded_var']);
+        self::mockProperty($testDBO, 'dbValues', ['id' => 1, 'name' => 'test name']);
+        $this->assertEquals($testDBO->id, 1);
+        $this->assertNotEquals($testDBO->name, 'test name edited');
+        $this->assertNull($testDBO->not_loaded_var);
+
+        $this->assertTrue($testDBO->isDBVariable('id'));
+        $this->assertFalse($testDBO->isDBVariable('regularProperty'));
+    }
+    
+
+    public static function mockProperty($object, string $propertyName, $value)
+    {
+        $reflectionClass = new \ReflectionClass($object);
+
+        $property = $reflectionClass->getProperty($propertyName);
+        $property->setAccessible(true);
+        $property->setValue($object, $value);
+        $property->setAccessible(false);
+    }
+}
