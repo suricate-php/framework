c26b5cba2ba556142d3407ba6db3d487ccf052ed
Mathieu LESNIAK <mathieu@lesniak.fr>
2019-01-20 20:32:59 +0100

    test helppers

--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -10,4 +10,47 @@ class HelperTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals(43, dataGet($dataArr, 'test-unknown', 43));
     }
 
+    public function testP()
+    {
+        $this->expectOutputString("<pre>\n1\n</pre><pre>\nArray\n(\n    [a] => 1\n)\n\n</pre>");
+        _p(1);
+
+        _p(['a' => 1]);
+    }
+
+    public function testHead()
+    {
+        $arr = ['a' => 1, 'b' => 2, 'c' => 3];
+        $this->assertSame(1, head($arr));
+    }
+
+    public function testLast()
+    {
+        $arr = ['a' => 1, 'b' => 2, 'c' => 3];
+        $this->assertSame(3, last($arr));
+    }
+
+    public function testFlatten()
+    {
+        $arr = ['a' => 1, 'b' => 2, 'c' => ['s1' => 3, 's2' => 4]];
+        $this->assertSame([1, 2, 3, 4], flatten($arr));
+    }
+
+    public function testValue()
+    {
+        $value = 1;
+        $this->assertSame(1, value($value));
+
+        $value = function() {
+            return 2;
+        };
+        $this->assertSame(2, value($value));
+    }
+
+    function testCamelCase()
+    {
+        $str = 'this_is_a_test';
+        $this->assertSame('ThisIsATest', camelCase($str));
+
+    }
 }
