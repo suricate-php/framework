cca4efa85240b24cbf381a0a7e32cb38a2840a41
Mathieu LESNIAK <mathieu@lesniak.fr>
2019-01-27 14:06:07 +0100

    more tests

--- tests/FormItemTest.php
+++ tests/FormItemTest.php
@@ -55,6 +55,63 @@ class FormItemTest extends \PHPUnit\Framework\TestCase
         );
     }
 
+    public function testInputButton()
+    {
+        $res = FormItem::button('my-input-name', "accentué", 'Mon input');
+        $this->assertEquals(
+            '<label for="my-input-name">Mon input</label><input type="button" name="my-input-name" id="my-input-name" value="accentu&eacute;"/>',
+            $res
+        );
+    }
+
+    public function testInputCheckbox()
+    {
+        $res = FormItem::checkbox('my-input-name', 2, true, 'Mon input');
+        $this->assertEquals(
+            '<label for="my-input-name">Mon input</label><input type="checkbox" name="my-input-name" id="my-input-name" value="2" checked="checked"/>',
+            $res
+        );
+
+        $res = FormItem::checkbox('my-input-name', 2, false, 'Mon input');
+        $this->assertEquals(
+            '<label for="my-input-name">Mon input</label><input type="checkbox" name="my-input-name" id="my-input-name" value="2"/>',
+            $res
+        );
+    }
+
+    public function testInputRadio()
+    {
+        $res = FormItem::radio('my-input-name', [1, 2, 3], 2, 'Mon input');
+        $this->assertEquals(
+            '<label>Mon input</label><div class="radio-list"><div class="radio-item"><label for="my-input-name-0">1</label><input type="radio" name="my-input-name" id="my-input-name-0" value="0"/></div><div class="radio-item"><label for="my-input-name-1">2</label><input type="radio" name="my-input-name" id="my-input-name-1" value="1"/></div><div class="radio-item"><label for="my-input-name-2">3</label><input type="radio" name="my-input-name" id="my-input-name-2" value="2" checked="checked"/></div></div>',
+            $res
+        );
+    }
+
+    public function testInputReset()
+    {
+        $res = FormItem::reset('val');
+        $this->assertEquals(
+            '<input type="reset" value="val"/>',
+            $res
+        );
+    }
+
+    public function testInputSelect()
+    {
+        $res = FormItem::select('my-input-name', [1, 2, 3], 2, 'Mon input');
+        $this->assertEquals(
+            '<label>Mon input</label><select name="my-input-name"><option value="0">1</option><option value="1">2</option><option value="2" selected>3</option></select>',
+            $res
+        );
+
+        $res = FormItem::select('my-input-name', ["é\"" => ['a', 'b'], 2, 3 => ['c', 'd']], 2, 'Mon input');
+        $this->assertEquals(
+            '<label>Mon input</label><select name="my-input-name"><optgroup label="&eacute;&quot;"><option value="0">a</option><option value="1">b</option></optgroup><option value="0">2</option><optgroup label="3"><option value="0">c</option><option value="1">d</option></optgroup></select>',
+            $res
+        );
+    }
+
     public function testInputFile()
     {
         $res = FormItem::file('my-input-name', "Mon input");
@@ -69,6 +126,15 @@ class FormItemTest extends \PHPUnit\Framework\TestCase
         $res = FormItem::hidden('my-input-name', "accentué");
         $this->assertEquals(
             '<input type="hidden" name="my-input-name" value="accentu&eacute;"/>',
+            $res
+        );
+    }
+
+    public function testInputImage()
+    {
+        $res = FormItem::image('my-input-name', "https://gsuite.google.com/img/icons/product-lockup.png");
+        $this->assertEquals(
+            '<input type="image" name="my-input-name" src="https://gsuite.google.com/img/icons/product-lockup.png"/>',
             $res
         );
     }
