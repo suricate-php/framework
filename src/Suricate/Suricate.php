bf1bc47f74ed21539523fc572e4d66af88b9c166
Mathieu LESNIAK <mathieu@lesniak.fr>
2019-08-05 23:31:45 +0200

    feat: declare eventdispatcher service

--- src/Suricate/Suricate.php
+++ src/Suricate/Suricate.php
@@ -21,6 +21,7 @@ namespace Suricate;
  * @method static \Suricate\Curl            Curl($newInstance = false)            Get instance of Curl service
  * @method static \Suricate\Database        Database($newInstance = false)        Get instance of Database service
  * @method static \Suricate\Error           Error($newInstance = false)           Get instance of Error service
+ * @method static \Suricate\EventDispatcher EventDispatcher($newInstance = false) Get instance of EventDispatcher service
  * @method static \Suricate\I18n            I18n($newInstance = false)            Get instance of I18n service
  * @method static \Suricate\Logger          Logger($newInstance = false)          Get instance of Logger service
  * @method static \Suricate\Request         Request($newInstance = false)         Get instance of Request service
@@ -56,6 +57,7 @@ class Suricate
         'Curl' => '\Suricate\Curl',
         'Database' => '\Suricate\Database',
         'Error' => '\Suricate\Error',
+        'EventDispatcher' => '\Suricate\EventDispatcher',
         'I18n' => '\Suricate\I18n',
         'Logger' => '\Suricate\Logger',
         'Request' => '\Suricate\Request',
