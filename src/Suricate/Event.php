addc22795c83617fe3d910cfb2c34bed01ede461
Mathieu LESNIAK <mathieu@lesniak.fr>
2019-08-05 23:32:10 +0200

    feat: event base class, define event type

--- src/Suricate/Event.php
+++ src/Suricate/Event.php
@@ -6,57 +6,13 @@ namespace Suricate;
 
 class Event
 {
-    protected $listeners = [];
-    protected $wildcardListeners = [];
+    /**
+     * @const string
+     */
+    const EVENT_TYPE = '';
 
-    private $sortedListeners = [];
-
-    public function listen($events, $listener, $priority = 0)
+    public static function getEventType(): string
     {
-        foreach ((array) $events as $event) {
-            if (strpos($event, '*') !== false) {
-            } else {
-                $this->listeners[$event][$priority][] = $listener;
-            }
-        }
-    }
-
-    public function fire($event, $eventData = [])
-    {
-        $impactedListeners = $this->getImpactedListeners($event);
-        foreach ($impactedListeners as $listener) {
-        }
-    }
-
-    public function subscribe($subscriber)
-    {
-        $subscriber->subscribe($this);
-
-        return $this;
-    }
-
-    protected function getImpactedListeners($eventName)
-    {
-        if (!isset($this->sortedListeners[$eventName])) {
-            $listeners = isset($this->listeners[$eventName])
-                ? $this->listeners[$eventName]
-                : [];
-            $listeners = array_merge(
-                $listeners,
-                $this->getImpactedWildcardListeners($listeners)
-            );
-
-            $listeners = krsort($listeners);
-            $this->sortedListeners[$eventName] = $listeners;
-        }
-
-        return $this->sortedListeners[$eventName];
-    }
-
-    protected function getImpactedWildcardListeners($events)
-    {
-        $listeners = [];
-
-        return $listeners;
+        return static::EVENT_TYPE;
     }
 }
