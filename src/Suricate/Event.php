cc0961ed43cd44f862ef553f25f7c100c54c419f
Mathieu LESNIAK <mathieu@lesniak.fr>
2019-08-10 16:49:30 +0200

    define event constructor

--- src/Suricate/Event.php
+++ src/Suricate/Event.php
@@ -6,13 +6,23 @@ namespace Suricate;
 
 class Event
 {
-    /**
-     * @const string
-     */
+    /** @const string event type*/
     const EVENT_TYPE = '';
 
+    /** @var mixed $payload Event payload */
+    protected $payload;
+    /**
+     * Get event type
+     *
+     * @return string
+     */
     public static function getEventType(): string
     {
         return static::EVENT_TYPE;
+    }
+
+    public function __construct($payload = null)
+    {
+        $this->payload = $payload;
     }
 }
