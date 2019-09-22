9104ad2c1722d1bc521591b8d2759c1958956d5c
Mathieu LESNIAK <mathieu@lesniak.fr>
2019-09-22 11:59:46 +0200

    chore: lint

--- src/Suricate/Middleware.php
+++ src/Suricate/Middleware.php
@@ -4,5 +4,5 @@ namespace Suricate;
 
 abstract class Middleware implements \Suricate\Interfaces\IMiddleware
 {
-    abstract function call(Request &$request, Request &$reponse);
+    abstract public function call(Request &$request, Request &$reponse);
 }
