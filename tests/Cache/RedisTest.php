36bca9b14ff288912d53b63a2ae0122f412d4ec1
Mathieu LESNIAK <mathieu@lesniak.fr>
2020-02-08 12:10:39 +0100

    test: add basic redis class tests

---
+++ tests/Cache/RedisTest.php
@@ -0,0 +1,37 @@
+<?php
+class RedisTest extends \PHPUnit\Framework\TestCase
+{
+    public function testConstruct()
+    {
+        $cacheFile = new \Suricate\Cache\Redis();
+        $this->assertEquals('localhost', $cacheFile->host);
+        $this->assertEquals(6379, $cacheFile->port);
+    }
+
+    public function testGetSetPort()
+    {
+        $cacheFile = new \Suricate\Cache\Redis();
+
+        $this->assertEquals(6379, $cacheFile->getPort());
+        $cacheFile->setPort(6380);
+        $this->assertEquals(6380, $cacheFile->getPort());
+    }
+
+    public function testGetSetHost()
+    {
+        $cacheFile = new \Suricate\Cache\Redis();
+
+        $this->assertEquals('localhost', $cacheFile->getHost());
+        $cacheFile->setHost('127.0.0.1');
+        $this->assertEquals('127.0.0.1', $cacheFile->getHost());
+    }
+
+    public function testGetSetExpiry()
+    {
+        $cacheFile = new \Suricate\Cache\Redis();
+
+        $this->assertEquals(3600, $cacheFile->getDefaultExpiry());
+        $cacheFile->setDefaultExpiry(60);
+        $this->assertEquals(60, $cacheFile->getDefaultExpiry());
+    }
+}
