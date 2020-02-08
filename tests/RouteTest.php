797e48f59aa737ddb11074f992910cba2b1a189d
Mathieu LESNIAK <mathieu@lesniak.fr>
2020-02-08 14:44:34 +0100

    test: add getter

--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -22,5 +22,7 @@ class RouteTest extends \PHPUnit\Framework\TestCase
         );
 
         $this->assertEquals($path, $route->getPath());
+        $this->assertEquals('any', $route->getMethod());
+        $this->assertEquals($name, $route->getName());
     }
 }
